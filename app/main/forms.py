a33e69f0e32a5409513d9ba5fb7200f76f9ec991
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-18 10:45:00 +0300

    ft:posts-form - initiliazes posts form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import DataRequired
 from ..models import User,Post,Comment,Share
 
 
-class BlogForm(FlaskForm):
-    title = StringField('Enter your Blog Title', validators=[DataRequired()])
-    author = StringField('Blog Author Name',)
+class PostsForm(FlaskForm):
+    title = StringField('Enter your Post Title', validators=[DataRequired()])
+    author = StringField('Post Author Name',)
     # blog_category = RadioField('Blog Category :', choices = [('Technology', 'Technology'),  ('Lifestyle', ' Lifestyle'), ('Farming', 'Farming'),('Vehicles & Transport' , 'Vehicles and Transport'), ('Fashion and Clothing', 'Fashion & Clothing', ('Education', 'Education'))], validators = [DataRequired()])
-    content = TextAreaField('Blog content')
-    submit = SubmitField('BLOG')
+    content = TextAreaField('Your Personal Story')
+    submit = SubmitField('Make A Post')
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,9 @@
-
-  
 from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
-# from ..requests import 
-# from .forms import 
-# from ..models import User
-# from .. import db,photos
+from..import db
+# from flask_login import login_required,current_user
+from .forms import PostsForm
+from ..models import User,Share,Comment,Post
 
 # from flask_login import login_required,current_user
 
@@ -14,3 +12,16 @@ def index():
 
 
     return render_template('index.html')
+
+#Posts form
+@main.route('/create_posts')
+def new_posts():
+    form = PostsForm()
+    if form.validate_on_submit():
+        
+    
+        
+        return redirect(url_for('main.index'))
+        
+    
+    return render_template('posts.html',form = form)
