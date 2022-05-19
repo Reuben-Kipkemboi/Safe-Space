530aee8e9e171bbd1bf1b7a37649cfbd58c0d7c4
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-19 20:03:54 +0300

    st:card-styling - optimize login and register page styling

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     # UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://norah:1234we@localhost/safe'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/dbsafe'
     # API_KEY = os.environ.get('API_KEY')
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
