6bc8b38849956d29c69a9bc97973717500dd7224
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-19 22:20:25 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,32 +1,25 @@
 import os
-
 class Config:
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     # UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/dbsafe'
-    # API_KEY = os.environ.get('API_KEY')
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/dbsafe'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/dbsafe'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 
 # class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
 
 class ProdConfig(Config):
-    pass
-
+    SQLALCHEMY_DATABASE_URI='postgresql://lqxssqojjiudkb:98705d02848004c95ff66533c98eaf6c171547a2b6adc96f98a36c5d0ba91cf8@ec2-54-204-56-171.compute-1.amazonaws.com:5432/da27vsvg57ieid'
 
 class DevConfig(Config):
-    
-    
+    pass
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-# 'test':TestConfig
+    'development':DevConfig,
+    'production':ProdConfig,
+
 }
--- manage.py
+++ manage.py
@@ -7,10 +7,8 @@ from  flask_migrate import Migrate, MigrateCommand
 
 
 from app.models import User, Post, Share, Comment
-
-
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 # app = create_app('test')
 
 manager = Manager(app)
@@ -20,12 +18,12 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
